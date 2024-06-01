9dcc170624196e59cf7a084120db264a356040a3
g;itchtonny <sirmwangitosh@gmail.com>
2024-06-01 11:44:44 +0300

    complete lab

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -38,8 +38,56 @@ test("checkbox appears as unchecked when user clicks a second time", () => {
 
 // Size select element
 
+test("select Size dropdown displays the user's selected value", () => {
+  render(<App />);
+
+  const selectSize = screen.getByLabelText(/select size/i);
+
+  userEvent.selectOptions(selectSize, "medium");
+
+  expect(selectSize).toHaveDisplayValue("Medium");
+
+  userEvent.selectOptions(selectSize, "large");
+
+  expect(selectSize).toHaveDisplayValue("Large");
+});
+
+
 // "Your Selection" text
+test("selecting options updates the 'Your selection' message", () => {
+  render(<App />);
+
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  const selectSize = screen.getByLabelText(/select size/i);
+
+  userEvent.click(addPepperoni);
+
+  expect(screen.getByText(/small pepperoni/i)).toBeInTheDocument();
+
+  userEvent.selectOptions(selectSize, "large");
+
+  expect(screen.getByText(/large pepperoni/i)).toBeInTheDocument();
+});
+
 
 // "Contact Info" text box
 
+test("the page shows information the user types into the contact form field", () => {
+  render(<App />);
+
+  const contact = screen.getByLabelText(/enter your email address/i);
+
+  userEvent.type(contact, "pizzafan@email.com");
+
+  expect(contact).toHaveValue("pizzafan@email.com");
+});
+
 // Submit Order button
+
+test("clicking the Place Order button displays a thank you message", () => {
+  render(<App />);
+
+  userEvent.click(screen.getByRole("button", { name: /submit order/i }));
+
+  expect(screen.getByText(/thanks for your order!/i)).toBeInTheDocument();
+});
